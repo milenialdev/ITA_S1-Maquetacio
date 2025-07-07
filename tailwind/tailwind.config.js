c8be7c5ca71a672f97f8a005cdc3e49f7cba0803
milenialdev <carlosbenitoandev@gmail.com>
2025-07-07 22:20:15 +0200

    Issues with folders and files solved and header created

---
+++ tailwind/tailwind.config.js
@@ -0,0 +1,10 @@
+   /** @type {import('tailwindcss').Config} */
+   module.exports = {
+    content: [
+      "./src/**/*.{html,js}", // Escanea todos los HTML y JS en src/
+    ],
+    theme: {
+      extend: {},
+    },
+    plugins: [],
+  }
